a1956ee91e0bf27eaba575eb4beab32bc0b3a196
Omar AbuRish <191020@ppu.edu.ps>
2022-01-16 15:44:39 +0200

    turn on + turn off

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const turnON = ()=> {
+    fetch('http://localhost:3000/on').then((res) => res.json()).then(data => console.log(data))
+  }
+
+  const turnOFF = ()=> {
+    fetch('http://localhost:3000/off').then((res) => res.json()).then(data => console.log(data))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={turnON}>
+        Turn ON
+      </button>
+
+      <button onClick={turnOFF}>
+        Turn OFF
+      </button>
     </div>
   );
 }
